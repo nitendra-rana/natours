e1bd53711b91500b95599571e52aa9d1b985e324
nitendra-rana <nitendrarana108@gmail.com>
2024-11-29 01:29:01 +0530

    dev vs prod error

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,9 +1,38 @@
-module.exports = (err, res, resp, next) => {
+const prodErrorResponse = (err) => {
+  if (err.isOperational) {
+    //OPERATIONAL ERROR :SEND TO CLIENT
+    return {
+      status: err.status,
+      message: err.message,
+    };
+  }
+  console.error(err);
+  return {
+    statusCode: 500,
+    status: 'error',
+    message: 'something went wrong please contact customer support.',
+  };
+};
+
+const devErrorResponse = (err) => ({
+  status: err.status,
+  message: err.message,
+  error: err,
+  stack: err.stack,
+});
+
+const sendErrorRes = (err, res) => {
+  let responseData = {};
+  if (process.env.NODE_ENV === 'development') {
+    responseData = devErrorResponse;
+  } else if (process.env.NODE_ENV === 'production') {
+    responseData = prodErrorResponse(err);
+  }
+  res.status(err.statusCode).json(responseData);
+};
+
+module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'errror';
-
-  resp.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  sendErrorRes(err, res);
 };
