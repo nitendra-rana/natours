68b88cd46dd2ae35b1fa36fe5935363de8406a56
nitendra-rana <nitendrarana108@gmail.com>
2024-11-30 16:10:36 +0530

    Duplicate and validation error

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -4,7 +4,17 @@ const handleCastErrorDb = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errorResponse.errmsg.match(/(["'])(\\?.)*?\1/)[0]; //Regular expression to get string form error.
+  const message = `Duplicate field value : ${value}. Please use another value.`;
+  return new AppError(message, 400);
+};
 
+const handleValidationError = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data.${errors.join('. ')}.`;
+  return new AppError(message, 400);
+};
 const prodErrorResponse = (err) => {
   if (err.isOperational) {
     //OPERATIONAL ERROR :SEND TO CLIENT
@@ -13,7 +23,7 @@ const prodErrorResponse = (err) => {
       message: err.message,
     };
   }
-  console.error(err);
+  // console.error(err);
   return {
     statusCode: 500,
     status: 'error',
@@ -34,9 +44,9 @@ const sendErrorRes = (err, res) => {
   if (process.env.NODE_ENV === 'development') {
     responseData = devErrorResponse(error);
   } else if (process.env.NODE_ENV === 'production') {
-    if (err.name === 'CastError') {
-      error = handleCastErrorDb(error);
-    }
+    if (err.name === 'CastError') error = handleCastErrorDb(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === 'ValidationError') error = handleValidationError(error);
     responseData = prodErrorResponse(error);
   }
   res.status(error.statusCode).json(responseData);
