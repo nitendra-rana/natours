c49f7d2fe962b2e7160481614d96adcd0dc50d51
nitendra-rana <nitendrarana108@gmail.com>
2024-11-30 16:53:52 +0530

    uncaught exception handeling added for app

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+/**
+ * stuff like DB config
+ * error handling
+ * env vars
+ * anything that is ot related to express/app we will put it here.
+ *
+ */
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+/**
+ * needs to be defined at top, because any error happening before this method,
+ * won't get caught in event uncaughtException.
+ */
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception ,shutting down the server.');
+  console.info(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
@@ -28,8 +45,9 @@ const server = app.listen(PORT, () => {
 
 process.on('unhandledRejection', (err) => {
   //unhandledRejection : will listen to all the unhandled rejected promises in  async code.
-  console.info(err.name, err.message);
   console.error('Unhandled rejection,shutting down the server.');
+  console.info(err);
+
   server.close(() => {
     /*
      *server.close:  will give time to finish all pending requests.
@@ -38,11 +56,3 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
-
-/**
- * stuff like DB config
- * error handling
- * env vars
- * anything that is ot related to express/app we will put it here.
- *
- */
