028eb7cf434d36bcfd445becec208f4f32b3c475
nitendra-rana <nitendrarana108@gmail.com>
2024-12-03 14:55:05 +0530

    Jwt authorization added to signup user, fixed signp api security

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,23 @@
 const User = require('../models/usersModal');
 const { catchAsync } = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const userDetails = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    photo: req.body.photo || '',
+  };
+  //! if we directly use req.body, then anyone can put nay data.
+  const newUser = await User.create(userDetails);
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
