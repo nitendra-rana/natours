6bbf646322f021d93b603941c2590b68e0ef49ab
nitendra-rana <nitendrarana108@gmail.com>
2024-12-20 23:41:20 +0530

    review Model added, file name fix

---
+++ src/models/reviewModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  review: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  tour: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'tour',
+    required: [true, 'Review must belong to a tour.'],
+  },
+  users: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'users',
+      required: [true, 'Review must given by a user.'],
+    },
+  ],
+});
+
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour,users ',
+    select: '-__v',
+  });
+
+  next();
+});
+
+const Review = mongoose.model('review', reviewSchema);
+
+module.exports = Review;
